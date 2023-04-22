9931add3e08d805731ad02eefcabaa60c0c9dd7f
Lair Martes <lairmartes@gmail.com>
2023-04-22 12:23:47 -0500

    Add validation to Result domain

--- src/main/java/com/gmail/lairmartes/shyftlab/result/domain/Result.java
+++ src/main/java/com/gmail/lairmartes/shyftlab/result/domain/Result.java
@@ -3,6 +3,9 @@ package com.gmail.lairmartes.shyftlab.result.domain;
 import com.gmail.lairmartes.shyftlab.course.entity.Course;
 import com.gmail.lairmartes.shyftlab.student.entity.Student;
 import com.gmail.lairmartes.shyftlab.result.enums.Score;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -13,20 +16,23 @@ public class Result {
 
     private long id;
 
-    @NonNull
-    private Student student;
+    @Min(value = 1, message = "Student ID is mandatory.")
+    private long studentId;
 
-    @NonNull
+    @Min(value = 1, message = "Course ID is mandatory.")
+    private long courseId;
+
+    @NotBlank(message = "Provide a score A, B, C, D, E or F.")
+    private String score;
+
+    private Student student;
     private Course course;
 
-    @NonNull
-    private Score score;
-
-    public com.gmail.lairmartes.shyftlab.result.entity.Result toEntity() {
+    public com.gmail.lairmartes.shyftlab.result.entity.Result toEntity(final Student student, final Course course) {
         return com.gmail.lairmartes.shyftlab.result.entity.Result.builder()
-                .student(this.student)
-                .course(this.course)
-                .score(this.score)
+                .student(student)
+                .course(course)
+                .score(Score.valueOf(this.score))
                 .build();
     }
 
@@ -34,7 +40,7 @@ public class Result {
         return Result.builder()
                 .student(entity.getStudent())
                 .course(entity.getCourse())
-                .score(entity.getScore())
+                .score(entity.getScore().name())
                 .build();
     }
 }
