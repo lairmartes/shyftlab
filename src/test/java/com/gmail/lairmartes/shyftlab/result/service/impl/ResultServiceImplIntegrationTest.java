18319162a35889dcd897a49717157358c47e3262
Lair Martes <lairmartes@gmail.com>
2023-04-22 17:46:10 -0500

    Add list results test

--- src/test/java/com/gmail/lairmartes/shyftlab/result/service/impl/ResultServiceImplIntegrationTest.java
+++ src/test/java/com/gmail/lairmartes/shyftlab/result/service/impl/ResultServiceImplIntegrationTest.java
@@ -4,6 +4,7 @@ import com.gmail.lairmartes.shyftlab.common.exception.RecordNotFoundException;
 import com.gmail.lairmartes.shyftlab.course.entity.Course;
 import com.gmail.lairmartes.shyftlab.course.repository.CourseRepository;
 import com.gmail.lairmartes.shyftlab.result.domain.Result;
+import com.gmail.lairmartes.shyftlab.result.enums.Score;
 import com.gmail.lairmartes.shyftlab.student.entity.Student;
 import com.gmail.lairmartes.shyftlab.student.repository.StudentRepository;
 import jakarta.validation.ConstraintViolation;
@@ -18,6 +19,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -65,31 +67,32 @@ class ResultServiceImplIntegrationTest {
                         .familyName("Naruto")
                         .birthDate(LocalDate.of(1993, 10, 10))
                         .email("un@email.com")
-                        .build()),
-                studentRepository.save(Student
-                        .builder()
-                        .firstName("Sakura")
-                        .familyName("Haruno")
-                        .birthDate(LocalDate.of(1993,12,13))
-                        .email("sn@email.com")
-                        .build()),
-                studentRepository.save(Student
-                        .builder()
-                        .firstName("Hinata")
-                        .familyName("Hyuga")
-                        .birthDate(LocalDate.of(1993,7,17))
-                        .email("sn@email.com")
                         .build())
         );
     }
 
     private List<Course> createCourses() {
         return List.of(
-                courseRepository.save(Course.builder().name("Kunai IV").build()),
                 courseRepository.save(Course.builder().name("Kage Bushin III").build()),
-                courseRepository.save(Course.builder().name("Kekkai Ninjutsu").build()),
-                courseRepository.save(Course.builder().name("Advanced Chakra Control").build())
+                courseRepository.save(Course.builder().name("Kekkai Ninjutsu").build())
         );
+    }
+
+    private void createResults(final List<Student> savedStudents, final List<Course> savedCourses) {
+
+        final var studentId1 = savedStudents.get(0).getId();
+        final var studentId2 = savedStudents.get(1).getId();
+        final var studentId3 = savedStudents.get(2).getId();
+
+        final var courseId1 = savedCourses.get(0).getId();
+        final var courseId2 = savedCourses.get(1).getId();
+
+        resultService.addResult(Result.builder().studentId(studentId1).courseId(courseId1).score("B").build());
+        resultService.addResult(Result.builder().studentId(studentId1).courseId(courseId2).score("C").build());
+        resultService.addResult(Result.builder().studentId(studentId2).courseId(courseId1).score("A").build());
+        resultService.addResult(Result.builder().studentId(studentId2).courseId(courseId2).score("B").build());
+        resultService.addResult(Result.builder().studentId(studentId3).courseId(courseId1).score("D").build());
+        resultService.addResult(Result.builder().studentId(studentId3).courseId(courseId2).score("F").build());
     }
 
     @Nested
@@ -139,12 +142,12 @@ class ResultServiceImplIntegrationTest {
         @Test
         void whenStudentDoesNotExist_thenThrowsRecordNotFoundException_andReturnsCorrectMessage() {
 
-            var databaseCourseList = createCourses();
+            var savedCourses = createCourses();
 
             final Result newResult = Result
                     .builder()
                     .studentId(10390L)
-                    .courseId(databaseCourseList.get(0).getId())
+                    .courseId(savedCourses.get(0).getId())
                     .score("B")
                     .build();
 
@@ -160,11 +163,11 @@ class ResultServiceImplIntegrationTest {
         @Test
         void whenCourseDoesNotExist_thenThrowsRecordNotFoundException_andReturnsCorrectMessage() {
 
-            var databaseStudentList = createStudents();
+            var savedStudents = createStudents();
 
             final Result newResult = Result
                     .builder()
-                    .studentId(databaseStudentList.get(0).getId())
+                    .studentId(savedStudents.get(0).getId())
                     .courseId(20985L)
                     .score("B")
                     .build();
@@ -179,7 +182,76 @@ class ResultServiceImplIntegrationTest {
         }
     }
 
-    @Test
-    void listAllResults() {
+    @Nested
+    class ListAllResults {
+
+        @Test
+        void whenAllEntitiesArePresent_thenReturnsAllResults() {
+
+            final var savedStudents = createStudents();
+            final var savedCourses = createCourses();
+
+            final var expectedResults = List.of(
+                    new CompareResults(savedStudents.get(0), savedCourses.get(0), "B"),
+                    new CompareResults(savedStudents.get(0), savedCourses.get(1), "C"),
+                    new CompareResults(savedStudents.get(1), savedCourses.get(0), "A"),
+                    new CompareResults(savedStudents.get(1), savedCourses.get(1), "B"),
+                    new CompareResults(savedStudents.get(2), savedCourses.get(0), "D"),
+                    new CompareResults(savedStudents.get(2), savedCourses.get(1), "F")
+            );
+
+            createResults(savedStudents, savedCourses);
+
+            final var actualResults = resultService.listAllResults().stream().map(CompareResults::create).toList();
+
+            assertTrue(actualResults.containsAll(expectedResults));
+        }
+
+        @Test
+        void whenRemovingStudent_thenDoesNotShowTheirResults() {
+
+            final var savedStudents = createStudents();
+            final var savedCourses = createCourses();
+
+            final var expectedResults = List.of(
+                    new CompareResults(savedStudents.get(0), savedCourses.get(0), "B"),
+                    new CompareResults(savedStudents.get(0), savedCourses.get(1), "C"),
+                    new CompareResults(savedStudents.get(2), savedCourses.get(0), "D"),
+                    new CompareResults(savedStudents.get(2), savedCourses.get(1), "F")
+            );
+
+            createResults(savedStudents, savedCourses);
+
+            studentRepository.delete(savedStudents.get(1));
+
+            final var actualResults = resultService.listAllResults().stream().map(CompareResults::create).toList();
+
+            assertTrue(actualResults.containsAll(expectedResults));
+        }
+
+    }
+
+    private static class CompareResults {
+        final long studentId;
+        final long courseId;
+        final String score;
+
+        private CompareResults(final Student student, final Course course, final String score) {
+            this.studentId = student.getId();
+            this.courseId = course.getId();
+            this.score = score;
+        }
+
+        private static CompareResults create(final Result result) {
+            return new CompareResults(result.getStudent(), result.getCourse(), result.getScore());
+        }
+
+        public boolean equals(Object o) {
+            if (!(o instanceof CompareResults)) return false;
+
+            return ( ((CompareResults)o).courseId == this.courseId
+                    && ((CompareResults)o).studentId == this.studentId
+                    && Objects.equals(((CompareResults) o).score, this.score));
+        }
     }
 }
