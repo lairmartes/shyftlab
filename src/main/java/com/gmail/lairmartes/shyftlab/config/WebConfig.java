e1da8daec42c152c51761fe9dccedf01d47cf717
Lair Martes <lairmartes@gmail.com>
2023-04-27 18:06:30 -0500

    Add DELETE method in allowed CORS

--- src/main/java/com/gmail/lairmartes/shyftlab/config/WebConfig.java
+++ src/main/java/com/gmail/lairmartes/shyftlab/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.gmail.lairmartes.shyftlab.config;
 
 import lombok.NonNull;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(final @NonNull CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods(CorsConfiguration.ALL);
     }
 }
